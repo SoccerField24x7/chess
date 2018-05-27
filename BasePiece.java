1ce6f2c0710c8464f79382dc3e0855aa456e4325
Jesse Quijano <jesse@quijano.net>
2018-05-26 17:53:04 -0700

    Begin fleshing out the methods required to manage movement. Add methods to return informational data.

--- BasePiece.java
+++ BasePiece.java
@@ -5,18 +5,48 @@
  * @author (your name here)
  * @version (version number or date here)
  */
-public class BasePiece implements Piece
+public abstract class BasePiece implements Piece
 {
-    String[] location;
-    int pieceType;
-    
-    BasePiece(int pieceType, String[] where) {
+    public Piece.PieceType pieceType;
+    public Piece.Color pieceColor;
+    public boolean canMoveBackward = true;
+    public int[][] validMoves; // first [] contains "move index", second [] contains: 0-column, 1-row
+    public int[] location; //index 0 = column, index 1 = row
+    public boolean hasMoved = false;
+    public int maxSpaces = 8; // how many spaces can the piece move? 8 = unlimited
+
+    BasePiece(Piece.PieceType pieceType, Piece.Color color, int[] where) {
         this.pieceType = pieceType;
+        this.pieceColor = color;
+        //this.location = new int[2]; //allocate memory
         this.location = where;
+
+        if(this.pieceType == Piece.PieceType.PAWN) {
+            this.canMoveBackward = false;
+            this.maxSpaces = 2;
+        }
+        if(this.pieceType == Piece.PieceType.KNIGHT) {
+            this.maxSpaces = 3;
+        }
     }
-    
-    public Boolean isValidMove(BasePiece piece, String[] move)
-    {
+
+    public boolean doMove(int[] location) {
+        this.location = location;
+        this.hasMoved = true;
+
+        if(this.pieceType == Piece.PieceType.PAWN) {
+            this.maxSpaces = 1;
+        }
+
         return true;
     }
+
+    public Piece.PieceType getPieceType() {
+        return this.pieceType;
+    }
+
+    public Piece.Color getPieceColor() {
+        return this.pieceColor;
+    }
+    
 }
